bf4174b312aeb1c272d8d405488fa3acb4d77abf
Efat-Khan <efatkhan.duet.cse@gmail.com>
2026-08-01 22:04:17 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from datetime import date
-
+from utils import add, subtract
 
 def main() -> None:
 	print("Efat Khan")
 	print(date.today().isoformat())
-
+	print(add(100, 3))
+	print(subtract(10, 40))
 
 if __name__ == "__main__":
 	main()
